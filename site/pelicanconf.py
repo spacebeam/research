8e03d3f5daa6ee47a460f6d5a0f7466abeb39826
Jean Chassoul <chassoul@gmail.com>
2017-05-21 09:38:51 -0400

    hmm

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Team Machine'
+AUTHOR = u'Nonsense Worlds'
 SITENAME = u'shaos.ws'
-SITEURL = ''
+SITEURL = 'https://shaos.ws'
 PATH = 'content'
 TIMEZONE = 'Etc/UTC'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 # Community
 LINKS = (('Nonsense Worlds', 'https://nonsense.ws/'),)
@@ -22,7 +19,7 @@ LINKS = (('Nonsense Worlds', 'https://nonsense.ws/'),)
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 # Social widget
-SOCIAL = (('cube', 'https://nonsense.ws'),
+SOCIAL = (('cube', 'https://shaos.ws/images/shaos-5.21-amd64.iso'),
 	('book', 'https://github.com/nonsensews/guide/wiki'),
 	('twitter', 'https://twitter.com/shaosws'),
 	('github', 'https://github.com/nonsensews'),

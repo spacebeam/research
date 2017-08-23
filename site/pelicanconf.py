25dd94a68d8946640f60746e8260b1d55f965b05
Jean Chassoul <chassoul@gmail.com>
2017-08-23 11:39:29 -0400

    hm

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -35,5 +35,5 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 # Configure pelican theme
 THEME = "pelican-shaos"
 
-BIO = "Antifragile distributed OS environment for kids of all ages"
+BIO = "Distributed OS environment for kids of all ages"
 PROFILE_IMAGE = "https://shaos.ws/theme/img/shaos.png"

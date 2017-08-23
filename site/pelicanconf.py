002f05bd34e35395b98438da6b5f6b7f4f34486b
Jean Chassoul <chassoul@gmail.com>
2017-08-23 13:33:30 -0400

    ok

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -35,5 +35,5 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 # Configure pelican theme
 THEME = "pelican-shaos"
 
-BIO = "Distributed workspace environment"
+BIO = "Distributed scientific workspace"
 PROFILE_IMAGE = "https://shaos.ws/theme/img/shaos.png"

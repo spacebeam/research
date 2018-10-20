6ff1dedc98c113b57de90e32f822ded0dad3c9ef
Jean Chassoul <jchassoul@gmail.com>
2018-10-20 11:26:35 -0500

    clean, clean, clean

---
+++ capuchino.org/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "capuchino" > /etc/hostname
+
+sed -i 's/$/ universe/' /etc/apt/source.list
+
+sed -i 's/$/ contrib/' /etc/apt/source.list
+
+apt update
+
+apt install --install-recommends tcpdump wget openssh-client openssh-server xserver-xorg-core xserver-xorg xinit pciutils usbutils ntfs-3g hfsprogs dosfstools pv network-manager-gnome network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-vpnc-gnome i3lock compton geany htop urlview mupdf e2fsprogs xfsprogs feh cmus pavucontrol vlc jfsutils ntfs-3g fuse gvfs gvfs-fuse fusesmb libnss-mdns gvfs-bin gvfs-backends dconf-service dconf-cli dconf-editor gksu xdg-utils lxrandr build-essential gnome-calculator gpicview xarchiver audacity git gnome-themes-standard thunderbird calendar-google-provider fonts-lyx python3-apt python3-dev python3-pip zsh lxappearance mpv python-wxgtk3.0 python3-venv linux-image-amd64 live-boot systemd-sysv curl vim less tmux i3 lightdm ranger pcmanfm transmission chromium rxvt-unicode-256color jq uuid-dev uuid odbcinst unixodbc unixodbc-dev make cmake libncurses5-dev protobuf-compiler libsdl2-dev libsdl2-image-dev pelican libasound2-dev libpulse-dev automake libtool inkscape evince pandoc gfortran luajit
+
+apt clean

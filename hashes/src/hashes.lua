c6ac1bab1800a743d5981bbe483f97625f198c59
Jean Chassoul <jchassoul@gmail.com>
2019-03-31 20:22:54 -0600

    clean, clean, clean

--- hashes/src/hashes.lua
+++ hashes/src/hashes.lua
@@ -1,6 +1,4 @@
--- Handles the creation of Riak indexes and SHA256 hashes
-local ooo = "1bac09f4db02ea35518f07c9b5fb6851d4ec87687bf7ef1c6f7403c9f0959338"
--- Turn it up, turn it up, turn it up, fucked up loud
+-- Handles the creation of system hashes
 local uuid = require("uuid")
 local sha2 = require("sha2")
 local turbo = require("turbo")
@@ -8,8 +6,8 @@ local argparse = require("argparse")
 local pgmoon = require("pgmoon")
 local ini = require("inifile")
 -- define service arguments
-local parser = argparse("server.lua", "handles the creation of system indexes and hashes")
-    parser:option("-c --config", "configuration file.", "animal.conf")
+local parser = argparse("server.lua", "handles the creation of system hashes")
+    parser:option("-c --config", "configuration file.", "hash.conf")
 -- parse resource arguments
 local args = parser:parse()
 -- get options from config file
@@ -29,46 +27,9 @@ local pg = pgmoon.new({
 -- System UUID
 local system_uuid = uuid()
 -- start things up, get the system uuid and shit.
-turbo.log.warning("Turn it up, turn it up, turn it up, fucked up loud")
-turbo.log.warning("Starting animal resource uuid: " .. system_uuid)
+turbo.log.warning("Starting hashes resource uuid: " .. system_uuid)
 -- postgres connect
 assert(pg:connect())
--- check index name
-function check_index(name)
-    local query = "SELECT uuid FROM indexes where name = '" .. name .. "'"
-    local result = pg:query(query)
-    return #result
-end
--- create new index
-function new_index(name, type)
-    local message = {}
-    local index_uuid = uuid()
-    local query = "INSERT INTO indexes(uuid, name, type) VALUES" ..
-                  "('".. index_uuid .. "', '" .. name ..
-                  "', '".. type .."')"
-    local result = pg:query(query)
-    if result['affected_rows'] == 1 then
-        message["uuid"] = index_uuid
-    else
-        message["error"] = "Error saving new data into indexes"
-    end
-    return message
-end
--- Index Handler Resource
-local IndexHandler = class("IndexHandler", turbo.web.RequestHandler)
--- HTTP POST handler
-function IndexHandler:post()
-    -- get a table from the raw json data
-    local data = self:get_json(true)
-    local message = {}
-    local check = check_index(data['name'])
-    if check == 0 then
-        message = new_index(data['name'], data['index_type'])
-    else
-        print('check index name ' .. data['name'] .. 'it appears to exist.')
-    end
-    self:write(message)
-end
 -- all trim strings
 function all_trim(s)
    return s:match( "^%s*(.-)%s*$" )
@@ -121,8 +82,7 @@ function HashHandler:post()
 end
 -- application resource
 local application = turbo.web.Application:new({
-    {"/indexes/", IndexHandler},
-    {"/hashes/", HashHandler}
+    {"/hashes/", HashHandler},
 })
 -- i/o application start listen on TCP port.
 application:listen(options['server']['port'])

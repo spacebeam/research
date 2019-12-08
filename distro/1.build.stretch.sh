8659ec47ec48498ab814d2ed724b82c3e5a620b8
Jean Chassoul <jchassoul@gmail.com>
2019-12-08 13:34:47 -0800

    clean, clean, clean

--- distro/1.build.stretch.sh
+++ distro/1.build.stretch.sh
@@ -38,7 +38,6 @@ apt install --install-recommends \
     intltool\
     libtool\
     libpcre2-dev\
-    libglib3.0-cil-dev\
     libgnutls28-dev\
     libgirepository1.0-dev\
     libxml2-utils\
@@ -164,6 +163,24 @@ apt clean
 apt remove yelp -y
 
 apt autoremove -y
+
+apt install --install-recommends\
+    libglib3.0-cil\ 
+    libglib3.0-cil-dev\
+ -y
+
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
+
+echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+cd vte-ng && ./autogen.sh && make && sudo make install
+cd ../termite && make && sudo make install
+sudo ldconfig
+sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+/usr/local/share/terminfo/x/xterm-termite \
+/lib/terminfo/x/xterm-termite
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
 
 dpkg --add-architecture i386
 

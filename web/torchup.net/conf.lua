c51ae784e266d15a654ef3919b48a4f875f08c3b
Jean Chassoul <jchassoul@gmail.com>
2020-12-01 19:42:14 -0600

    clean, clean, clean

--- web/torchup.net/conf.lua
+++ web/torchup.net/conf.lua
@@ -3,5 +3,7 @@ function love.conf(t)
     t.appendidentity = false
     t.version = "11.1"
     t.console = false
-    
+    t.window.title = "Torch Up"
+    t.window.width = 1280
+    t.window.height = 720
 end
--- web/torchup.net/main.lua
+++ web/torchup.net/main.lua
@@ -1,57 +1,56 @@
--- Include Simple Tiled Implementation into project
-local sti = require "./lib/sti/sti"
+local sti = require("./lib/sti/sti")
+local bump = require("./lib/bump/bump")
+
+local message = ''
+
+local player = {
+    x = 50,
+    lastY = 50,
+    y = 50,
+    w = 8,
+	h = 8,
+	speed = 0.85,
+    yAcc = 0,
+    xAcc = 0,
+    vel = 4,
+    dx = 0,
+	dy = 0
+}
+
+local world = bump.newWorld(32)
 
 function love.load()
-	-- Load map file
-	map = sti("maps/WindSpirit.lua")
+	-- Load tiled map file
+	map = sti("maps/WindSpirit.lua", {'bump'})
+	map:bump_init(world)
 
-	-- Create new dynamic data layer called "Sprites" as the 8th layer
+	-- Create new dynamic data layer called "Sprites" as the 3th layer
 	local layer = map:addCustomLayer("Sprites", 3)
-
-	-- Get player spawn object
-	local player
+	-- Get spawn object
+	local spawn
 	for k, object in pairs(map.objects) do
 		if object.name == "Player" then
-			player = object
+			spawn = object
 			break
 		end
 	end
-
 	-- Create player object
 	local sprite = love.graphics.newImage("asset/gfx/portrait.png")
+
 	layer.player = {
 		sprite = sprite,
-		x      = player.x,
-		y      = player.y,
+		x      = spawn.x,
+		y      = spawn.y,
 		ox     = sprite:getWidth() / 2,
 		oy     = sprite:getHeight() / 1.35
 	}
 
-	-- Add controls to player
-	layer.update = function(self, dt)
-		-- 96 pixels per second
-		local speed = 96 * dt
+	player["x"] = layer.player.x
+	player["y"] = layer.player.y
+	player["ox"] = layer.player.ox
+	player["oy"] = layer.player.oy
 
-		-- Move player up
-		if love.keyboard.isDown("w", "up") then
-			self.player.y = self.player.y - speed
-		end
-
-		-- Move player down
-		if love.keyboard.isDown("s", "down") then
-			self.player.y = self.player.y + speed
-		end
-
-		-- Move player left
-		if love.keyboard.isDown("a", "left") then
-			self.player.x = self.player.x - speed
-		end
-
-		-- Move player right
-		if love.keyboard.isDown("d", "right") then
-			self.player.x = self.player.x + speed
-		end
-	end
+	world:add(player, player.x, player.y, player.w, player.h)
 
 	-- Draw player
 	layer.draw = function(self)
@@ -76,9 +75,52 @@ function love.load()
 	map:removeLayer("Object Layer 1")
 end
 
+function gamekeypressed(key)
+    if key == "escape" then 
+        love.event.quit()
+    end
+end
+
+function love.keypressed(key)
+    gamekeypressed(key)
+end
+
 function love.update(dt)
 	-- Update world
 	map:update(dt)
+
+	player.lastY = player.y
+	player.yAcc = 1
+	player.dx = 0
+
+	-- Move player up
+	if love.keyboard.isDown("w", "up") then
+		player.yAcc = -player.speed
+	end
+
+	-- Move player down
+	if love.keyboard.isDown("s", "down") then
+		player.yAcc = player.speed
+	end
+
+	-- Move player left
+	if love.keyboard.isDown("a", "left") then
+		--player.x = player.x - walk_speed
+		player.xAcc = -player.speed
+	end
+
+	-- Move player right
+	if love.keyboard.isDown("d", "right") then
+		--player.x = player.x + walk_speed
+		player.xAcc = player.speed
+	end
+
+	player.dx = player.xAcc * player.vel
+    player.dy = player.yAcc * player.vel
+
+    player.x, player.y, cols, len = world:move(player, player.x + player.dx, player.y + player.dy)
+
+	player.xAcc, player.yAcc = 0, player.yAcc * 0.45
 end
 
 function love.draw()
@@ -88,14 +130,20 @@ function love.draw()
 	local screen_height = love.graphics.getHeight() / scale
 
 	-- Translate world so that player is always centred
-	local player = map.layers["Sprites"].player
-	local tx = math.floor(player.x - screen_width  / 2)
-	local ty = math.floor(player.y - screen_height / 2)
+	local prayer = map.layers["Sprites"].player
+	local tx = math.floor(prayer.x - screen_width  / 2)
+	local ty = math.floor(prayer.y - screen_height / 2)
 
-	-- Transform world
-	--love.graphics.scale(scale)
-	--love.graphics.translate(-tx, -ty)
+	local dx = math.floor(player.x - screen_width  / 2)
+	local dy = math.floor(player.y - screen_height / 2)
+
+	--local dx = player.x - (love.graphics.getWidth() / 2) / scale
+	--local dy = player.y - (love.graphics.getHeight() / 2) / scale
+	
+	map:draw(-dx, -dy, scale, scale)
+
+	love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
 
 	-- Draw world
-	map:draw(-tx, -ty, scale, scale)
+	--map:draw(-tx, -ty, scale, scale)
 end

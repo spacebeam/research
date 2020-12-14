1f68f4806c7f9f3df2e8d09d413260732b6e229d
Jean Chassoul <jchassoul@gmail.com>
2020-12-14 12:03:55 -0600

    clean, clean, clean

--- web/torchup.net/lib/lovetoys/lovetoys.lua
+++
@@ -1,8 +0,0 @@
--- Getting folder that contains our src
-local folderOfThisFile = (...):match("(.-)[^%/%.]+$")
-
-local lovetoys = require(folderOfThisFile)
-
-print('require(\'lovetoys.lovetoys\') is deprecated. Use require(\'lovetoys\') instead.')
-
-return lovetoys

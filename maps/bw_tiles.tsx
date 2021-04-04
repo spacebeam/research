43de41aeec3294554104269e7366e0485fd4d33e
Jean Chassoul <jchassoul@gmail.com>
2021-04-04 09:37:36 -0600

    clean, clean, clean

---
+++ maps/bw_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="bw_tiles" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="gfx/bw_tiles.png" width="64" height="64"/>
+</tileset>
